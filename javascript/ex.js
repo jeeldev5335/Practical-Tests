93c36e915429ab63d1556650bf6e38199eb25edd
Jeel Patel <jeelpatel6294@gmail.com>
2022-06-15 14:26:37 +0530

    First commit

---
+++ javascript/ex.js
@@ -0,0 +1,21 @@
+// javascript variables
+
+//number variable
+var num1 = 10;
+var num2 = 20;
+//console.log(num1+num2);
+
+//string variable
+var str1 = 'Jeel'
+var str2 = "Patel"
+console.log(str1,str2)
+
+// var float = 2.3;
+// console.log(typeof float);
+
+// javascript functions
+function avg(a,b) {
+    return (a+b)/2;
+}
+c = avg(22,18);
+//console.log(c);
